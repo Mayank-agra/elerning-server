c0cdf91137b7b78ef3b622a0b1d7cdc31d5b0e11
Mayank Agrawal <149233491+Mayank-agra@users.noreply.github.com>
2025-03-01 23:45:22 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,8 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import sendMail from "../middlewares/sendMail.js";
-import TryCatch from "../middlewares/TryCatch.js";
+import sendMail from "../middlewares/sendmail.js";
+import TryCatch from "../middlewares/trycatch.js";
 
 export const register = TryCatch(async (req, res) => {
   const { email, name, password } = req.body;
